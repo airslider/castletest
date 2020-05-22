d9f3f7dc64b99d4cd715ecbc37f2a38394a5a45c
nikolindima <nikolindima@gmail.com>
2020-05-23 01:04:12 +0300

    Initial commit

---
+++ castletest/App Delegate/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  castletest
+//
+//  Created by Dmitriy Nikolin on 23/05/2020.
+//  Copyright © 2020 dnikolin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+  
+
+}
+
